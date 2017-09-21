02e756ce2ba5a671e6a40ed887dc97df7ffb15eb
carlosrios89 <carlosrios89@gmail.com>
2017-09-21 15:48:11 -0500

    final tests

--- src/main/java/com/prodigious/unalautomation/HomePage.java
+++ src/main/java/com/prodigious/unalautomation/HomePage.java
@@ -3,131 +3,234 @@ package com.prodigious.unalautomation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class HomePage {
 
-    // Init of Driver
+
     WebDriver driver;
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    // Init of variables
-
-
-    // URL for testing
     public static final  String PAGE_URL = "http://puppies.herokuapp.com/";
 
 
-    // Map of elements
 
-    //***** Hero *****
-    WebElement heroLogo = driver.findElement(By.xpath("//*[@id=\"sb_top\"]/div/a/img")); // Error
-    WebElement heroSearch = driver.findElement(By.xpath("//*[@id=\"header\"]/input")); // It isn't working
-    WebElement heroEmail = driver.findElement(By.xpath("//*[@id=\"header\"]/a/img")); // Page doesn't exist
-
-
-    //***** Menu *****
-    WebElement menuAdoptPuppies = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-    WebElement MenuLern = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-    WebElement MenuAnimalShelters = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-    WebElement MenuClassifieds = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-    WebElement MenuMessageBoards = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-    WebElement MenuPetNews = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-
-
-
-    //***** Puppy List - Page 1 *****
-    WebElement detailsBrook = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-    WebElement detailsHanna = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/div/div[4]/form/div/input"));
-    WebElement detailsMaggieMae = driver.findElement(By.xpath("//*[@id=\"content\"]/div[4]/div/div[4]/form/div/input"));
-    WebElement detailsRubySue = driver.findElement(By.xpath("//*[@id=\"content\"]/div[5]/div/div[4]/form/div/input"));
-
-    //***** Puppy List - Page 2 *****
-    WebElement detailsSparky = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-    WebElement detailsSpud = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/div/div[4]/form/div/input"));
-    WebElement detailsTipsy = driver.findElement(By.xpath("//*[@id=\"content\"]/div[4]/div/div[4]/form/div/input"));
-    WebElement detailsTopsy = driver.findElement(By.xpath("//*[@id=\"content\"]/div[5]/div/div[4]/form/div/input"));
-
-    //***** Puppy List - Page 3 *****
-    WebElement detailsTwinkie = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input"));
-
-    //***** Pagination Buttons below Puppy List - *****
-    WebElement buttonPrevious = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/a[1]"));
-    WebElement buttonOne = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/a[2]"));
-    WebElement buttonTwo = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/a[3]"));
-    WebElement buttonThree = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/em"));
-    WebElement buttonNext = driver.findElement(By.xpath("//*//*[@id=\"content\"]/div[3]/span"));
-
-    //***** Adopt a Puppy - Page ****** HomePage
-
-
-    //***** Learn - Page ****** Error
-
-
-    //***** Animal Shelters - Page ****** Error
-
-
-    //***** Classifieds - Page ****** Error
-
-
-    //***** Message Boards - Page ****** Error
-
-
-    //***** Pet News - Page ****** Error
-
-    //***** Brook Step 1 - View Details ***** Verify:  image, copy , CTAs
-    WebElement brookImage = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/img"));
-    WebElement brookH2 = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/h2"));
-    WebElement brookH3 = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/h3"));
-    WebElement brookDescriptionCopy = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/p"));
-    WebElement brookPriceCopy = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/span"));
-    WebElement brookAdoptMe = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/form/div/input[1]"));
-    WebElement brookReturn= driver.findElement(By.xpath("//*[@id=\"content\"]/a"));
+    @FindBy (how = How.XPATH,using = "//*[@id=\"sb_top\"]/div/a/img")
+            WebElement heroLogo;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"header\"]/input")
+            WebElement HeroSearch;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"header\"]/a/img")
+            WebElement heroEmail;
+    @FindBy (how = How.XPATH,using = "//*[@id=\"header\"]/input")
+            WebElement searchBar;
+    @FindBy (how = How.XPATH,using = "//*[@id=\"header\"]/a/img")
+            WebElement mailBox;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"content\"]/div[2]/div/div[4]/form/div/input")
+            WebElement detailsBrook;
+    @FindBy (how = How.XPATH,using = "//*[@id=\"content\"]/div[3]/div/div[4]/form/div/input")
+            WebElement detailsHanna;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"content\"]/div[2]/div/form/div/input[1]")
+            WebElement brookAdoptMe;
+    @FindBy (how = How.XPATH, using ="//*[@id=\"carrier\"]")
+            WebElement brookAdoptAdditionalCarrier;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"vet\"]")
+            WebElement brookAdoptAdditionalVet;
+    @FindBy (how = How.XPATH, using ="//*[@id=\"content\"]/div[2]/form[1]/div/input")
+            WebElement brookAdoptCompleteAdoption;
+    @FindBy (how = How.XPATH,using ="//*[@id=\"content\"]/div[2]/form[2]/div/input[1]")
+            WebElement brookAdoptAnotherPuppy;
+    @FindBy (how = How.XPATH,using = "//*[@id=\"content\"]/div[2]/form[3]/div/input[2]")
+            WebElement brookAdoptChange;
+    @FindBy (how = How.XPATH,using ="//*[@id=\"order_name\"]")
+            WebElement FormName;
+    @FindBy (how = How.XPATH,using ="//*[@id=\"order_address\"]")
+            WebElement FormAddress;
+    @FindBy (how = How.XPATH,using ="//*[@id=\"order_email\"]")
+            WebElement FormEmail;
+    @FindBy (id = "order_pay_type")
+            WebElement FormPayType;
+    @FindBy (how = How.XPATH,using = "//*[@id=\"order_pay_type\"]/option[3]")
+            WebElement FormpayTypeOption3;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"new_order\"]/div[6]/input")
+            WebElement FormPlaceOrder;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"collar\"]")
+            WebElement brookAdoptAdditionalCollar;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"toy\"]")
+            WebElement brookAdoptAdditionalToy;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"content\"]/a")
+            WebElement brookReturn;
+    @FindBy (how = How.XPATH,using = "//*[@id=\"sb_top\"]/div/a/img")
+            WebElement Homepage404;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"content\"]/div[2]/div/form/div/input[1]")
+            WebElement spudAdoptMe;
+    @FindBy (how = How.XPATH, using ="//*[@id=\"carrier\"]")
+            WebElement spudAdoptAdditionalCarrier;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"vet\"]")
+            WebElement spudAdoptAdditionalVet;
+    @FindBy (how = How.XPATH, using ="//*[@id=\"content\"]/div[2]/form[1]/div/input")
+            WebElement spudAdoptCompleteAdoption;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"content\"]/div[6]/a[4]")
+            WebElement NextPageButton;
+    @FindBy (how = How.XPATH, using = "//*[@id=\"content\"]/div[3]/div/div[4]/form/div/input")
+            WebElement detailsSpud;
 
 
-    //***** Brook - Adopt Me - Page ******
+    public void AdoptionProcess(String name, String address, String email){
+
+        new WebDriverWait(driver, 10);
+        detailsBrook.click();
+        new WebDriverWait(driver, 3);
+        brookAdoptMe.click();
+        new WebDriverWait(driver, 4);
+        brookAdoptAdditionalCarrier.click();
+        brookAdoptAdditionalVet.click();
+        brookAdoptCompleteAdoption.click();
+        new WebDriverWait(driver, 3);
+        FormName.sendKeys(name);
+        FormAddress.sendKeys(address);
+        FormEmail.sendKeys(email);
+        FormPayType.click();
+        new WebDriverWait(driver, 3);
+        FormpayTypeOption3.click();
+        FormPlaceOrder.click();
+        WebElement success = new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"notice\"]")));
+        Assert.assertTrue(success.getText().contains("Thank you for adopting a puppy!"));
+        driver.close();
+
+    }
+
+    public void AssertSearchBar(String search){
+
+        new WebDriverWait(driver,10);
+        searchBar.click();
+        searchBar.sendKeys(search);
+        Assert.assertTrue(searchBar.getText().contains(search));
+        driver.close();
+
+    }
+
+    public void AdoptionChangeMind(){
+
+        new WebDriverWait(driver, 10);
+        detailsBrook.click();
+        new WebDriverWait(driver, 3);
+        brookAdoptMe.click();
+        new WebDriverWait(driver, 4);
+        brookAdoptChange.click();
+        driver.switchTo().alert().accept();
+        new WebDriverWait(driver, 2);
+        driver.switchTo().alert().accept();
+        new  WebDriverWait(driver, 10);
+        driver.getCurrentUrl().equalsIgnoreCase(PAGE_URL);
+        driver.close();
+
+    }
+
+    public void AdoptTwoPuppies(String name, String address, String email){
+
+        new WebDriverWait(driver, 10);
+        detailsBrook.click();
+        new WebDriverWait(driver, 3);
+        brookAdoptMe.click();
+        new WebDriverWait(driver, 4);
+        brookAdoptAnotherPuppy.click();
+        new WebDriverWait(driver,5);
+        detailsHanna.click();
+        new WebDriverWait(driver,5);
+        brookAdoptMe.click();
+        new WebDriverWait(driver, 3);
+        brookAdoptAdditionalCarrier.click();
+        brookAdoptAdditionalVet.click();
+        brookAdoptCompleteAdoption.click();
+        new WebDriverWait(driver, 3);
+        FormName.sendKeys(name);
+        FormAddress.sendKeys(address);
+        FormEmail.sendKeys(email);
+        FormPayType.click();
+        new WebDriverWait(driver, 3);
+        FormpayTypeOption3.click();
+        FormPlaceOrder.click();
+        WebElement success = new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"notice\"]")));
+        Assert.assertTrue(success.getText().contains("Thank you for adopting a puppy!"));
+        driver.close();
+
+    }
+
+    public void FormError() {
+
+        new WebDriverWait(driver, 10);
+        detailsBrook.click();
+        new WebDriverWait(driver, 3);
+        brookAdoptMe.click();
+        new WebDriverWait(driver, 4);
+        brookAdoptAdditionalCollar.click();
+        brookAdoptAdditionalToy.click();
+        brookAdoptCompleteAdoption.click();
+        new WebDriverWait(driver, 3);
+        FormPlaceOrder.click();
+        WebElement formFieldsError = new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"error_explanation\"]/h2")));
+        Assert.assertTrue(formFieldsError.getText().contains("5 errors prohibited this order from being saved:"));
+        driver.close();
+
+    }
+
+    public void DetailsReturntoList() {
+
+        new WebDriverWait(driver, 10);
+        detailsBrook.click();
+        new WebDriverWait(driver, 3);
+        brookReturn.click();
+        new WebDriverWait(driver, 3);
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "http://puppies.herokuapp.com/");
+        driver.close();
+
+    }
+
+    public void PageNotFound(){
+
+        new WebDriverWait(driver, 10);
+        detailsBrook.click();
+        new WebDriverWait(driver, 2);
+        Homepage404.click();
+        new WebDriverWait(driver, 2);
+        WebElement success = new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/h1")));
+        Assert.assertTrue(success.getText().contains("The page you were looking for doesn't exist."));
+        driver.close();
+
+    }
 
 
-    //***** Return To List - Functionality ****** Go to Home
-    WebElement brookAdoptTitle = driver.findElement(By.xpath("//*[@id=\"content\"]/h2"));
+    public void AdoptionPageTwo(String name, String address, String email){
 
-    WebElement brookAdoptImage = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr[1]/td[1]/img"));
-    WebElement brookAdoptName = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr[1]/td[2]/h2"));
-    WebElement brookAdoptDescription = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr[1]/td[3]/h2"));
-    WebElement brookAdoptPrice = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr[1]/td[4]/h2"));
-
-    WebElement brookAdoptAdditional = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr[2]/td[2]/h3"));
-    WebElement brookAdoptAdditionalCollar = driver.findElement(By.xpath("//*[@id=\"collar\"]"));
-    WebElement brookAdoptAdditionalToy = driver.findElement(By.xpath("//*[@id=\"toy\"]"));
-    WebElement brookAdoptAdditionalCarrier = driver.findElement(By.xpath("//*[@id=\"carrier\"]"));
-    WebElement brookAdoptAdditionalVet = driver.findElement(By.xpath("//*[@id=\"vet\"]"));
-
-    WebElement brookAdoptTotal= driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr[7]/td[3]/h2"));
-
-    WebElement brookAdoptCompleteAdoption = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/form[1]/div/input"));
-    WebElement brookAdoptAnotherPuppy = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/form[2]/div/input[1]"));
-    WebElement brookAdoptChange = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/form[3]/div/input[2]"));
-
-
-    //***** Brook - Complete the adoption - Page ****** Form
-    WebElement FormTitle = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/fieldset/legend"));
-    WebElement FormName = driver.findElement(By.xpath("//*[@id=\"order_name\"]"));
-    WebElement FormAddress= driver.findElement(By.xpath("//*[@id=\"order_address\"]"));
-    WebElement FormEmail = driver.findElement(By.xpath("//*[@id=\"order_email\"]"));
-    WebElement FormPayType = driver.findElement(By.xpath("//*[@id=\"order_pay_type\"]"));
-    WebElement FormPayTypeCheck = driver.findElement(By.xpath("//*[@id=\"order_pay_type\"]/option[2]"));
-    WebElement FormPayTypeCredit = driver.findElement(By.xpath("//*[@id=\"order_pay_type\"]/option[3]"));
-    WebElement FormPayTypePurchase = driver.findElement(By.xpath("//*[@id=\"order_pay_type\"]/option[4]"));
-    WebElement FormPlaceHolder = driver.findElement(By.xpath("//*[@id=\"new_order\"]/div[6]/input"));
-    WebElement FormPlaceHolderError = driver.findElement(By.xpath("//*[@id=\"error_explanation\"]"));
-    WebElement FormPlaceHolderSuccess = driver.findElement(By.xpath("//*[@id=\"notice\"]")); //Homepage
+        new WebDriverWait(driver, 3);
+        NextPageButton.click();
+        new WebDriverWait(driver, 3);
+        detailsSpud.click();
+        new WebDriverWait(driver, 3);
+        spudAdoptMe.click();
+        new WebDriverWait(driver, 4);
+        spudAdoptAdditionalCarrier.click();
+        spudAdoptAdditionalVet.click();
+        spudAdoptCompleteAdoption.click();
+        new WebDriverWait(driver, 3);
+        FormName.sendKeys(name);
+        FormAddress.sendKeys(address);
+        FormEmail.sendKeys(email);
+        FormPayType.click();
+        new WebDriverWait(driver, 3);
+        FormpayTypeOption3.click();
+        FormPlaceOrder.click();
+        WebElement success = new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"notice\"]")));
+        Assert.assertTrue(success.getText().contains("Thank you for adopting a puppy!"));
+        driver.close();
+    }
 
 
-    //***** Brook - Adopt Another Puppy - Page ****** HomePage
-
-
-    //***** Brook - Adopt Change your mind - Page ****** Popup: Cancel (Stay on the same page) or Ok (Go to HomePage)
-    WebElement popupCancel = driver.findElement(By.xpath(""));
-    WebElement popupOk = driver.findElement(By.xpath(""));
 }
--- src/test/java/com/prodigious/unalautomation/PagesTest.java
+++ src/test/java/com/prodigious/unalautomation/PagesTest.java
@@ -1,4 +1,80 @@
 package com.prodigious.unalautomation;
 
+import com.prodigious.unalautomation.Helper.BrowserFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import static com.prodigious.unalautomation.HomePage.PAGE_URL;
+
 public class PagesTest {
+
+    @Test
+    public void adoptionTest(){
+
+        WebDriver driver= BrowserFactory.startBrowser("chrome",PAGE_URL);
+
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.AdoptionProcess("John Doe", "5141 3rd St, Omaha NB 32422", "john@doe.net");
+    }
+
+    @Test
+    public void assertSearchBar(){
+        WebDriver driver= BrowserFactory.startBrowser("chrome",PAGE_URL);
+
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.AssertSearchBar("hannah");
+    }
+
+    @Test
+    public void adoptionChangeMind(){
+        WebDriver driver= BrowserFactory.startBrowser("chrome",PAGE_URL);
+
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.AdoptionChangeMind();
+    }
+
+    @Test
+    public void adoptTwoPuppies(){
+        WebDriver driver= BrowserFactory.startBrowser("chrome",PAGE_URL);
+
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.AdoptTwoPuppies("John Doe", "5141 3rd St, Omaha NB 32422", "john@doe.net");
+    }
+
+    @Test
+    public void formTest(){
+
+        WebDriver driver= BrowserFactory.startBrowser("chrome",PAGE_URL);
+
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.FormError();
+    }
+
+    @Test
+    public void returnTest(){
+
+        WebDriver driver= BrowserFactory.startBrowser("chrome",PAGE_URL);
+
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.DetailsReturntoList();
+    }
+
+    @Test
+    public void PageNotFoundTest(){
+
+        WebDriver driver = BrowserFactory.startBrowser("firefox",PAGE_URL );
+
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.PageNotFound();
+    }
+
+    @Test
+    public void AdoptionPageTwoTest(){
+
+        WebDriver driver = BrowserFactory.startBrowser("safari", PAGE_URL);
+
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.AdoptionPageTwo("Luis Test", "9571 Congress St, Schenectady NY 12345", "luis@test.net");
+    }
 }
